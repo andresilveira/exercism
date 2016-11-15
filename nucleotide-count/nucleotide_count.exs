8eeb0b0a9a0d388951a3678882b2bf1908237a68
Andre Herculano <aherculano@babbel.com>
2016-11-15 13:24:04 +0100

    add nucleotide-count exercise

---
+++ nucleotide-count/nucleotide_count.exs
@@ -0,0 +1,53 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a NucleotideCount strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count([char], char) :: non_neg_integer
+  def count(strand, nucleotide) do
+    count(strand, nucleotide, 0)
+  end
+
+  def count([head|tail], nucleotide = head, acumulator) do
+    count(tail, nucleotide, acumulator + 1)
+  end
+
+  def count([_|tail], nucleotide, acumulator) do
+    count(tail, nucleotide, acumulator)
+  end
+
+  def count([], _, acumulator) do
+    acumulator
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram([char]) :: map
+  def histogram(strand) do
+    initial_histogram = %{?A => 0, ?T => 0, ?C => 0, ?G => 0}
+    _histogram(strand, initial_histogram)
+  end
+
+  def _histogram([head|tail], acumulator) do
+    _histogram(tail, %{acumulator| head => acumulator[head] + 1})
+  end
+
+  def _histogram([], acumulator) do
+    acumulator
+  end
+end
